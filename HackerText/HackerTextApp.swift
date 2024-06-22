f70d1c63a7b7a457e49abb886b3b05de97cbf83b
Radname <104667795+Radionsss@users.noreply.github.com>
2024-06-22 23:01:20 +0500

    .
    .

---
+++ HackerText/HackerTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerTextApp.swift
+//  HackerText
+//
+//  Created by Radion Kufeld on 21.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct HackerTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
